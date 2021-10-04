c24d31c1456da5da5fd593b3f9f3b83b0fce7b73
leehari-ute <nhai30928@gmail.com>
2021-10-04 18:04:00 +0700

    [Hai: new] login register verify function

--- app/Models/Index.Model.js
+++ app/Models/Index.Model.js
@@ -1,12 +1,12 @@
-const Account = require("./account.model");
-const Branch = require("./branch.model");
+const Account = require("./Account.Model");
+const Branch = require("./Branch.Model");
 const Cart = require("./Cart.Model");
 const Coupon = require("./Coupon.Model");
 const MobileBrand = require("./MobileBrand.Model");
 const MobileProduct = require("./MobileModel.Model");
 const Order = require("./Order.Model");
 const Product = require("./Product.Model");
-const User = require("./user.model");
+const User = require("./User.Model");
 
 module.exports = {
   Account,
