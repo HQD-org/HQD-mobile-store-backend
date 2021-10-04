1e143dea644e71d3b6804505a636f938ff56c134
leehari-ute <nhai30928@gmail.com>
2021-10-04 08:53:24 +0700

    complete model

---
+++ app/Common/RoutePath.js
@@ -0,0 +1,41 @@
+exports.AUTH_PATH = {
+  CHANGE_PASSWORD: "change-password",
+  FORGOT_PASSWORD: "forgot-password",
+  LOGIN: "login",
+  REGISTER: "register",
+  VERIFY: "verify",
+};
+
+exports.BRANCH_PATH = {
+  CREATE: "create",
+  // DELETE: "delete",
+  FIND_BY_NAME: "find-by-name",
+  GET_ALL: "get-all",
+  UPDATE: "update",
+};
+
+exports.MOBILE_BRAND_PATH = {
+  CREATE: "create",
+  // DELETE: "delete",
+  FIND_BY_NAME: "find-by-name",
+  GET_ALL: "get-all",
+  UPDATE: "update",
+};
+
+exports.MOBILE_MODEL_PATH = {
+  CREATE: "create",
+  // DELETE: "delete",
+  FIND_BY_NAME: "find-by-name",
+  GET_ALL: "get-all",
+  UPDATE: "update",
+};
+
+exports.PRODUCT_PATH = {
+  CREATE: "create",
+  FILTER_ALL: "filter-all",
+  FILTER_BY_BRAND: "filter-by-brand",
+  FILTER_BY_MODEL: "filter-by-model",
+  GET_ALL: "get-all",
+  SEARCH: "search",
+  UPDATE: "update",
+};
