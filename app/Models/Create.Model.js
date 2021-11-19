509900c6f3d5473b077f238b5ab01cd9127c1cb9
leehari-ute <nhai30928@gmail.com>
2021-11-19 10:41:21 +0700

    [Hai: upgrade] change response data filter mobile model

--- app/Models/Create.Model.js
+++ app/Models/Create.Model.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const createSchema = (schema) => {
-  const model = new Schema(schema, { timestamps: true });
-  return model;
+  return new Schema(schema, { timestamps: true });
 };
 
 module.exports = { createSchema };
