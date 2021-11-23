68c6eb3f1c32dcde4825568fcd0502a38ace81d0
leehari-ute <nhai30928@gmail.com>
2021-11-23 22:17:57 +0700

    [Hai: update] add updateProfile, edit filter and get all user

--- app/Routes/User.Route.js
+++ app/Routes/User.Route.js
@@ -1,15 +1,16 @@
-
-
+const { USER_PATH } = require("../Common/RoutePath");
 const controller = require("../Controllers/User.Controller");
 const express = require("express");
 const { schema } = require("../Validations/User.Validation");
 const { validateBody } = require("../Validations/Validation");
-const { validateQuery } = require("../Validations/Validation");
 const { verifyToken } = require("../Middlewares/Token.Middleware");
 const router = express.Router();
 
-router.route('/update').post([validateBody(schema.update) ,verifyToken],controller.handleUpdate);
-router.route('/get-all-user').get(controller.handleFindAllUser);
-router.route('/search-by-name').get([validateQuery(schema.search)],controller.handleSearchUserByName);
-module.exports = router
+router
+  .route(`/${USER_PATH.UPDATE}`)
+  .post(
+    [validateBody(schema.updateProfile), verifyToken],
+    controller.handleUpdateProfile
+  );
 
+module.exports = router;
--- app/Services/Auth.Service.js
+++ app/Services/Auth.Service.js
@@ -6,7 +6,10 @@ const { sendPassword, sendOtp } = require("./Mail.Service");
 
 const changePassword = async (idUser, oldPassword, newPassword) => {
   try {
-    const account = await Account.findOne({ idUser: idUser });
+    const account = await Account.findOne({}).populate({
+      path: "idUser",
+      match: { _id: idUser },
+    });
     if (!account)
       return {
         message: {
@@ -90,15 +93,11 @@ const forgotPassword = async (email) => {
 
 const getAuth = async (idUser) => {
   try {
-    const account = await Account.findOne({ idUser: idUser });
+    const account = await Account.findOne({}).populate({
+      path: "idUser",
+      match: { _id: idUser },
+    });
     if (!account)
-      return {
-        success: false,
-        message: "Unauthenticated",
-        status: HTTP_STATUS_CODE.FORBIDDEN,
-      };
-    const user = await User.findById(idUser);
-    if (!user)
       return {
         success: false,
         message: "Unauthenticated",
@@ -106,7 +105,7 @@ const getAuth = async (idUser) => {
       };
     return {
       data: {
-        ...user.toObject(),
+        user: account.idUser,
         role: account.role,
         idBrand: account.idBrand || "",
       },
@@ -167,7 +166,6 @@ const login = async (username, password) => {
       };
     }
 
-    console.log(`log at: => Auth.Service.js => line 143 => login success `);
     return {
       message: {
         ENG: "Login Successfully",
@@ -181,7 +179,6 @@ const login = async (username, password) => {
       status: HTTP_STATUS_CODE.OK,
     };
   } catch (error) {
-    console.log(`log at: => Auth.Service.js => line 161 => error: `, error);
     return {
       success: false,
       message: error.message,
