4bdbfda75f0e6662905f763aaddc85232174f9c9
vinhduc5984 <phungvinhduc3@gmail.com>
2021-12-13 11:34:46 +0700

    fix paypal

--- app/Controllers/Payment.Controller.js
+++ app/Controllers/Payment.Controller.js
@@ -3,23 +3,22 @@ const paypal = require("paypal-rest-sdk");
 const { Cart, Product ,  Order, } = require("../Models/Index.Model");
 const { HTTP_STATUS_CODE } = require("../Common/Constants");
 const { sendError, sendSuccess } = require("./Controller");
+const queryString = require('query-string');
 
   const OrderPaypal = async(req, res, next)=> {
     const { price } = req.body;
-    console.log(req.body.token.id);
-    console.log("price",price);
     const dollar = price/23100;
     const dollar2f = parseFloat(dollar.toFixed(2));
     const idUser = req.body.token.id;
     const idBranch = req.body.idBranch;
     const coupon = req.body.coupon;
-    const receiver = req.body.receiver;
-    const phone = req.body.phone;
-    const address = req.body.address;
-    const receiveAt = req.body.receiveAt;
-    const timeReceive = req.body.timeReceive;
-    const status = req.body.status;
-    const message = req.body.message;
+    const receiver = req.body.receiveInfo.receiver;
+    const phone = req.body.receiveInfo.phone;
+    const address = req.body.receiveInfo.address;
+    const receiveAt = req.body.receiveInfo.receiveAt;
+    const timeReceive = req.body.receiveInfo.timeReceive;
+    //const status = req.body.status;
+    const message = req.body.receiveInfo.message;
     const reqQuery = queryString.stringify({
       idUser,
       idBranch,
@@ -29,10 +28,12 @@ const { sendError, sendSuccess } = require("./Controller");
       address,
       receiveAt,
       timeReceive,
-      status,
+     // status,
       message,
+      dollar2f
     });
 
+    console.log("info: "+ reqQuery);
 
     const create_payment_json = {
       intent: "sale",
@@ -40,7 +41,7 @@ const { sendError, sendSuccess } = require("./Controller");
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: `http:///localhost:8080/payment/success?price=${dollar2f}?${reqQuery}`,
+        return_url: `http:///localhost:8080/payment/success?${reqQuery}`,
         cancel_url: "http://localhost:8080/payment/cancel",
       },
       transactions: [
@@ -94,7 +95,7 @@ const { sendError, sendSuccess } = require("./Controller");
   }
 
   const PaymentSuccess = async(req, res, next) =>{
-    let{
+    const{
       idUser,
       idBranch,
       coupon,
@@ -103,21 +104,22 @@ const { sendError, sendSuccess } = require("./Controller");
       address,
       receiveAt,
       timeReceive,
-      status,
+     // status,
       message,
+      dollar2f
     } = req.query;
     const payerId = req.query.PayerID;
     const paymentId = req.query.paymentId;
-    const price = req.query.price;
-    const dollar = price/23100;
-    const dollar2f = parseFloat(dollar.toFixed(2));
+    //const price = req.query.price;
+    const price = req.query.dollar2f;
+    console.log(price);
     const execute_payment_json = {
       payer_id: payerId,
       transactions: [
         {
           amount: {
             currency: "USD",
-            total: `${price}`,
+            total: `${dollar2f}`,
           },
         },
       ],
@@ -141,7 +143,7 @@ const { sendError, sendSuccess } = require("./Controller");
           try{
             const cart = await Cart.findOne({user:idUser});
             if(!cart){
-              res.status(400).send({
+              res.status(404).send({
                 data:"",
                 error:"Your cart is not exsit",
                 message:{
@@ -152,6 +154,7 @@ const { sendError, sendSuccess } = require("./Controller");
             }
             const products = cart.products; // lấy list sản phẩm
             let totalPrice = 0; // lấy tổng tiền
+            let idB = idBranch || "61a23e0527b5b90016616975";
             for(const p of products){
                 totalPrice = totalPrice + p.price; 
                 let quantityP = p.quantity;  
@@ -163,7 +166,7 @@ const { sendError, sendSuccess } = require("./Controller");
                  if(item.name ===colorP)
                  {
                      const quantityInfo = item.quantityInfo.map((quantity)=>{
-                         if(quantity.idBranch === idBranch)
+                         if(quantity.idBranch === idB)
                          {
                              quantity.quantity = quantity.quantity - quantityP;
                              return quantity;
@@ -178,23 +181,34 @@ const { sendError, sendSuccess } = require("./Controller");
             product.color = r;
             await product.save();
             }
+            const reInfo = {
+              receiver:receiver,
+              phone:phone,
+              address:address,
+              receiveAt:receiveAt,
+              timeReceive:timeReceive,
+              status:"online",
+              message:message,
+            }
+
+            console.log("reInfo:"+reInfo);
+            console.log("receiver:"+reInfo.receiver);
             const newOrder = new Order({
                 products: products,
+                idBranch:idB,
                 totalPrice: totalPrice,
                 coupon: coupon,
                 user: idUser,
-                receiveInfo: {
-                  receiver,
-                  phone,
-                  address,
-                  receiveAt,
-                  timeReceive,
-                  status,
-                  message,
-                },
+                receiveInfo: reInfo,
                 status : "wait"
             });
+
             await newOrder.save();
+
+              // xóa cart cũ
+              //await Cart.deleteOne({user:idUser});
+            await Cart.findOneAndUpdate({ user: idUser }, { $set: { products: [] } });
+            console.log(newOrder);
             res.status(200).send({
               data:"",
               message:{
@@ -202,8 +216,7 @@ const { sendError, sendSuccess } = require("./Controller");
                 VN:"Thanh toán thành công",
               }
             });
-            // xóa cart cũ
-            await Cart.deleteOne({user:idUser});
+          
     
         }catch(err){
           res.status(400).send({
