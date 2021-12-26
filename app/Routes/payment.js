62072ec26345ba71358b7517d1d68ef0b8ccd97c
leehari-ute <nhai30928@gmail.com>
2021-12-26 12:28:04 +0700

    [Hai: update] paypal api for web

--- app/Routes/payment.js
+++ app/Routes/payment.js
@@ -1,12 +1,13 @@
-
 const express = require("express");
 const router = express.Router();
-const PaymentController = require('../Controllers/Payment.Controller');
+const PaymentController = require("../Controllers/Payment.Controller");
 const { verifyToken } = require("../Middlewares/Token.Middleware");
 
-router.post('/order',[verifyToken], PaymentController.OrderPaypal);
-router.get('/success', PaymentController.PaymentSuccess);
-router.get('/cancel', PaymentController.CancelPayment);
-router.post('/refund', PaymentController.RefundPayment);
+router.post("/order", [verifyToken], PaymentController.OrderPaypal);
+router.get("/success", PaymentController.PaymentSuccess);
+router.get("/success-web", PaymentController.PaymentSuccessWeb);
+router.get("/cancel", PaymentController.CancelPayment);
+router.get("/cancel-web", PaymentController.CancelPaymentWeb);
+router.post("/refund", PaymentController.RefundPayment);
 
 module.exports = router;
